5d1948e3e53e6ef8f0651bf7a0210eedbdf085d3
mphvictrsepng-rgb <mphvoctrsepng@gmail.com>
2026-04-22 23:24:24 +0200

    Here is my completed test. Additionally, I uploaded screen recordings of the terminal output as evidence instead of screen shots. I chose to take screen recordings because I also wanted to display what happens when the conditions pass or fail all in one setting.

--- password_validator.py
+++ password_validator.py
@@ -1,5 +1,5 @@
 # --- QUESTION 1: THE BASIC GATEKEEPER ---
-"""
+
 password = input('Question 1 - Set your password here: ')
 if len(password) < 6:
     print('Too Short')
@@ -7,15 +7,16 @@ elif password == '123456' or password == 'password':
     print('Too Simple')
 else:
     print('Validating...')
-"""
 
-# Logic
+# My logic
 """
-The first thing I did was to ask the user for a password input using the input() function. The value off the password is stored in the variable named password.
-Then I used an if statement to check if the length of the password is less than the 6 characters as required. If it is, I print "Too Short".
-Next, I used an elif statement to check if the password is either "123456" or "password". If it is, I print "Too Simple".
+The first thing I did was to ask the user for a password input using the input() function. The value off the password is stored
+ in the variable named password. Then I used an if statement to check if the length of the password is less than the 6 characters 
+ as required. If it is, I print "Too Short". Next, I used an elif statement to check if the password is either "123456" or "password".
+ If it is, I print "Too Simple".
 Finally, if neither of those conditional tests are met or fail, I print "Validating...".
 """
+
 # --- QUESTION 2: THE MULTI-FACTOR SIMULATION ---
 
 pwd = input("Question 2 - Enter Password: ")
@@ -27,14 +28,40 @@ elif pwd == 'Admin' and pin != 9999:
 else:
     print('Access Denied')
 
-# Logic
+# My logic
 """
-Here the first thing I did was to prompt the user to insert their passward (the value ( a string) is stored in the pwd variable) and then I asked them to insert their
-PIN (the value (as an interger) is stored in the pin variable). Then I used the if statement to test if the password (pwd) matches 'Admin' as well as to check if
-pin is equal to or matches '9999' using the and operator, as well as the == comparison operator to compare values against variables. 
-If both of the conditions pass the test, then I print 'Supperuser Access'. 
+Here the first thing I did was to prompt the user to insert their passward (the value ( a string) is stored in the pwd variable)
+and then I asked them to insert their PIN (the value (as an interger) is stored in the pin variable). Then I used the if statement
+ to test if the password (pwd) matches 'Admin' as well as to check if pin is equal to or matches '9999' using the and operator, as well 
+ as the == comparison operator to compare values against variables. If both of the conditions pass the test, then I print 'Supperuser Access'. 
 
 Additionally, I used the elif statement to test if pwd is equal to 'Admin' but the pin is not equal to '9999' using the != comparison operator.
-If the conditions pass the test, then I print 'Invalid PIN'. 
-Lastly, I used the else statement to catch all other situations where the passwprd is wrong(not 'Admin') and pin is wrong (not '9999') and print out 'Access denied'.
+If the conditions pass the test, then I print 'Invalid PIN'. Lastly, I used the else statement to catch all other situations where the
+ passwprd is wrong(not 'Admin') and pin is wrong (not '9999') and print out 'Access denied'.
+"""
+
+# --- QUESTION 3: THE COMPLEXITY BOSS ---
+
+master_pwd = input("Question 3 - Create Master Password: ")
+#if len(master_pwd) >= 10 and len(master_pwd) <= 20:
+requirements = [len(master_pwd) >= 10 and len(master_pwd) <= 20, 'login' not in master_pwd, master_pwd.startswith('!') or master_pwd.startswith('@')]
+if all(requirements):
+    print('Password Robust!')
+else:
+    print('Weakness Detected')
+
+# My logic
+"""
+This one took me a while to figure it out. 
+The first thing I did was to ask the user to create a master password using the input() function and store the value in the master_pwd 
+variable. Then I tried to write an if statement for each of the 3 rules but I couldn't figure out how to do that for all the 3 rules
+separately. Hence I decided to create a list called requirements and store all the 3 rules in that list. The firt thing I did in the
+list was to check if the length of the master password is between 10 and 20 using len() function (>= 10 and <=20). The 2nd item in the
+list is to check if the word 'login' is not in the master passowrd using the not in operator (if not in, condition is true and vice versa
+if its in the master password, which would be false). The 3rd item in the list is to check if master password starts with "!" or "@" using
+the startwith() methd; I used the or operator to check if neither of the condition is true. 
+
+Finally, I used if staetemt to check if all the items in the requirements list are true using the all() function and if they are, I print
+(Password robust!). I used the else statement to catch all other situations where the entered master password fails any of the 3 rules
+ in the list, then the all() fucntion will return false and I print('Weakness Detected').
 """
--- questions.py
+++ questions.py
@@ -4,7 +4,7 @@
 # 3. If it's "123456" OR "password", print "Too Simple".
 # 4. Otherwise, print "Validating...".
 
-#password = input("Question 1 - Set a password: ")
+password = input("Question 1 - Set a password: ")
 # Write logic here:
 
 # --- QUESTION 2: THE MULTI-FACTOR SIMULATION ---
@@ -13,3 +13,18 @@
 # 2. If the password is "Admin" but the PIN is wrong, print "Invalid PIN".
 # 3. If the password is wrong, print "Access Denied".
 
+pwd = input("Question 2 - Enter Password: ")
+pin = int(input("Question 2 - Enter 4-digit PIN: "))
+# Write logic here:
+
+# --- QUESTION 3: THE COMPLEXITY BOSS ---
+# Rules for a "Master Password":
+# 1. It must be between 10 and 20 characters long.
+# 2. It must NOT contain the word "login" (in any case).
+# 3. It must start with the character "!" or "@".
+#
+# If all 3 rules are met, print "Password Robust!".
+# If any rule is failed, print "Weakness Detected".
+
+master_pwd = input("Question 3 - Create Master Password: ")
+# Write logic
